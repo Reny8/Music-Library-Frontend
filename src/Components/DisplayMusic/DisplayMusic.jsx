f6086e5d7f11fd9b3c7ff301cbf9e8d155b4cdb3
Reny Reyes <renyreyes8@hotmail.com>
2022-04-28 10:58:39 -0400

    Added the like,delete,update buttons

--- src/Components/DisplayMusic/DisplayMusic.jsx
+++ src/Components/DisplayMusic/DisplayMusic.jsx
@@ -2,27 +2,35 @@ import "./DisplayMusic.css";
 const DisplayMusic = (props) => {
   return (
     <div>
-      <table className='table table-striped'>
+      <table className="table table-striped">
         <thead>
           <tr>
-            <th scope='col'>ALBUM</th>
-            <th scope='col'>ARTIST</th>
-            <th scope='col'>TITLE</th>
-            <th scope='col'>GENRE</th>
-            <th scope='col'>RELEASE DATE</th>
+            <th scope="col">ALBUM</th>
+            <th scope="col">ARTIST</th>
+            <th scope="col">TITLE</th>
+            <th scope="col">GENRE</th>
+            <th scope="col">RELEASE DATE</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody className="content">
           {props.songs.map((song) => {
             return (
-              <tr key={song.id}>
+              <tr key={song.id} scope="row">
                 <td>
-                  <img src={song.album_image}/> {song.album}
+                  <img src={song.album_image} />
+                  {song.album}
                 </td>
                 <td>{song.artist}</td>
                 <td>{song.title}</td>
                 <td>{song.genre}</td>
                 <td>{song.release_date}</td>
+                <td>
+                  <div className='button'>
+                    <button>Like {song.likes}</button>
+                    <button>Update</button>
+                    <button>Delete</button>
+                  </div>
+                </td>
               </tr>
             );
           })}
