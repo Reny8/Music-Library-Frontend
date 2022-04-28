217b685c56b44a8ff0c9f6f32fb5eb1379cb66f7
Reny Reyes <renyreyes8@hotmail.com>
2022-04-28 17:43:37 -0400

    Added the final styles to the form button

--- src/Components/AddSong/AddSong.jsx
+++ src/Components/AddSong/AddSong.jsx
@@ -1,55 +1,83 @@
-import React, { useState} from 'react';
-import './AddSong.css'
+import React, { useState } from "react";
+import "./AddSong.css";
 const AddSong = (props) => {
-const [album_image, setAlbumImage] = useState("")
-const [album, setAlbum] = useState("")
-const [artist, setArtist] = useState("")
-const [title, setTitle] = useState("")
-const [genre, setGenre] = useState("")
-const [release_date, setReleaseDate] = useState("")
+  const [album_image, setAlbumImage] = useState("");
+  const [album, setAlbum] = useState("");
+  const [artist, setArtist] = useState("");
+  const [title, setTitle] = useState("");
+  const [genre, setGenre] = useState("");
+  const [release_date, setReleaseDate] = useState("");
 
-function Add(event) {
-    event.preventDefault()
+  function Add(event) {
+    event.preventDefault();
     let newSong = {
-        title: title,
-        artist: artist,
-        album: album,
-        release_date: release_date,
-        genre: genre,
-        likes: 0,
-        album_image:album_image,
-    }
-    props.newSong(newSong)
-    setAlbumImage("")
-    setAlbum("")
-    setArtist("")
-    setTitle("")
-    setGenre("")
-    setReleaseDate("")
-}
+      title: title,
+      artist: artist,
+      album: album,
+      release_date: release_date,
+      genre: genre,
+      likes: 0,
+      album_image: album_image,
+    };
+    props.newSong(newSong);
+    setAlbumImage("");
+    setAlbum("");
+    setArtist("");
+    setTitle("");
+    setGenre("");
+    setReleaseDate("");
+  }
 
+  return (
+    <form className="create" onSubmit={Add}>
+      <div className="form-group">
+        <h3>Add A Song Here</h3>
+        <label>Album Image</label>
+        <input
+          value={album_image}
+          className="form-control"
+          placeholder="Add a url of your image..."
+          onChange={(event) => setAlbumImage(event.target.value)}
+        />
+        <label>Album</label>
+        <input
+          value={album}
+          className="form-control"
+          onChange={(event) => setAlbum(event.target.value)}
+        />
+        <label>Artist</label>
+        <input
+          value={artist}
+          className="form-control"
+          onChange={(event) => setArtist(event.target.value)}
+        />
+        <label>Title</label>
+        <input
+          value={title}
+          className="form-control"
+          onChange={(event) => setTitle(event.target.value)}
+        />
+        <label>Genre</label>
+        <input
+          value={genre}
+          className="form-control"
+          onChange={(event) => setGenre(event.target.value)}
+        />
+        <label>Release Date</label>
+        <input
+          value={release_date}
+          className="form-control"
+          placeholder="yyyy-mm-dd"
+          onChange={(event) => setReleaseDate(event.target.value)}
+        />
+      </div>
+      <div className="around-button">
+        <button class="add-song" type="submit">
+          Add
+        </button>
+      </div>
+    </form>
+  );
+};
 
-
-    return (
-       <form className= 'create' onSubmit={Add}>
-        <div className= 'form-group'>
-            <h3>Add A Song Here</h3>
-            <label>Album Image</label>
-            <input value={album_image} className= 'form-control' placeholder='Add a url of your image...'onChange= {(event) => setAlbumImage(event.target.value)}/>
-            <label>Album</label>
-            <input value={album} className= 'form-control' onChange= {(event) => setAlbum(event.target.value)}/>
-            <label>Artist</label>
-            <input value={artist} className= 'form-control' onChange= {(event) => setArtist(event.target.value)}/>
-            <label>Title</label>
-            <input value={title} className= 'form-control' onChange= {(event) => setTitle(event.target.value)}/>
-            <label>Genre</label>
-            <input value={genre} className= 'form-control' onChange= {(event) => setGenre(event.target.value)}/>
-            <label>Release Date</label>
-            <input value={release_date} className= 'form-control' placeholder="yyyy-mm-dd"onChange= {(event) => setReleaseDate(event.target.value)}/>
-        </div>
-        <button type="submit">Add</button>
-       </form>
-    );
-}
- 
 export default AddSong;
